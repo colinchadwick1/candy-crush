865910b6ac7c93c365e7b9b27b3f7343cd4f4082
macbook <macbook@macbooks-MacBook-Pro.local>
2021-08-12 14:41:01 +0100

    multicoloured squares

--- src/app.js
+++ src/app.js
@@ -3,10 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 8
     const squares = []
 
+    const candyColor= [
+      'red',
+      'yellow',
+      'orange',
+      'purple',
+      'green',
+      'blue',
+
+    ]
+
 
     function createBoard() {
         for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
+            let randomColor = Math.floor(Math.random() * candyColor.length)
+            square.style.backgroundColor = candyColor[randomColor]
             grid.appendChild(square)
             squares.push(square)
         }
