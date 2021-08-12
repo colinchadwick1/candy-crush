417bcbdb14b4abd99eafac6826af54b1df30ac7e
macbook <macbook@macbooks-MacBook-Pro.local>
2021-08-12 14:56:34 +0100

    divs now draggable and console logs which color is being dragged

--- src/app.js
+++ src/app.js
@@ -19,6 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
             const square = document.createElement('div')
             let randomColor = Math.floor(Math.random() * candyColor.length)
             square.style.backgroundColor = candyColor[randomColor]
+            square.setAttribute('draggable', true)
+            square.setAttribute('id', i)
             grid.appendChild(square)
             squares.push(square)
         }
@@ -26,5 +28,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
 createBoard()
 
+let colorBeingDragged 
+
+//drag the candies
+squares.forEach(square => square.addEventListener('dragstart', dragStart))
+squares.forEach(square => square.addEventListener('dragend', dragEnd))
+squares.forEach(square => square.addEventListener('dragover', dragOver))
+squares.forEach(square => square.addEventListener('dragenter', dragEnter))
+squares.forEach(square => square.addEventListener('dragdrop', dragDrop))
+
+function dragStart () {
+  colorBeingDragged = this.style.backgroundColor
+  console.log(colorBeingDragged)
+  console.log(this.id, 'dragStart')
+}
+
+function dragEnd () {
+  console.log(this.id, 'dragEnd')
+}
+
+function dragOver () {
+  console.log(this.id, 'dragOver')
+}
+
+function dragEnter () {
+  console.log(this.id, 'dragEnter')
+}
+
+function dragDrop () {
+  console.log(this.id, 'dragDrop')
+}
 
 })
